2b47eee0fb4e269c1ba3d1ced05496f00dd43fd3
Roman <dr.orlov@yandex.ru>
2026-06-22 17:23:44 +0700

    chore: remove debug script with hardcoded Windows paths

--- query.py
+++
@@ -1,5 +0,0 @@
-import os
-path1 = "Z:/polymarket-bot/vault/database.sqlite"
-path2 = "Z:/vault/database.sqlite"
-print(f"{path1} exists: {os.path.exists(path1)}")
-print(f"{path2} exists: {os.path.exists(path2)}")
