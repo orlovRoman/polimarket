fa527564c4f376cb3968e5f1015772472fb09b56
Roman <dr.orlov@yandex.ru>
2026-07-08 23:27:49 +0700

    Cleanup: remove debug and scratch scripts from repo root

--- scratch_log.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-out = subprocess.check_output(['sudo', 'journalctl', '-u', 'polymarket-bot.service', '--since', '4 hours ago', '--no-pager'], text=True)
-for line in out.splitlines():
-    if 'SCOUT' in line:
-        print(line)
